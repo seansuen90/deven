2467feb3f7c5ccf6f6e1705153363e3fbd53649a
seansuen90 <seansuen90@gmail.com>
2026-01-29 15:53:52 +0000

    .

---
+++ app/api/events/route.ts
@@ -0,0 +1,36 @@
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import Event from "@/database/event.model"
+import {v2 as cloudinary} from 'cloudinary'
+
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+        const formData = await req.formData();
+        let event;
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            return NextResponse.json({message: 'Invalid JSON data format'}, { status: 400});
+        }
+        const file = formData.get('image') as File; // why do we get 'image from formData and not event object
+        if (!file) return NextResponse.json({ message: 'Image file is required'}, { status: 400 });
+        const arrayBuffer = await file.arrayBuffer(); // what does this do
+        const buffer = Buffer.from(arrayBuffer); // why do we create another buffer?
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvent'}, (error, results) => {
+                if (error) return reject(error);
+                resolve(results);
+            }).end(buffer); // what does end(buffer) do, why buffer last and error results first
+        }); // what does this promise do
+        event.image = (uploadResult as {secure_url: string}).secure_url
+
+        const createdEvent = await Event.create(event);
+        return NextResponse.json({ message: 'Event Created Successfully', event: createdEvent}, {status: 201});
+
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({message: 'Event Creation Failed', error: e instanceof Error ? e.message: 'Unknown'}, {status: 500})
+    }
+}
