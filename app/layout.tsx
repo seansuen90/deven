3d67bb475b5b6ddbd244c6f36772d1aa5ddd52db
seansuen90 <seansuen90@gmail.com>
2026-01-30 16:16:50 +0000

    added event details page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,18 +32,15 @@ export default function RootLayout({
         <Navbar />
         <div className='absolute inset-0 top-0 z-[-1] min-h-screen'>
           <LightRays
-            raysOrigin="top-center-offset"
-            raysColor="#5dfeca"
-            raysSpeed={1}
-            lightSpread={0.5}
-            rayLength={3}
-            followMouse={true}
-            mouseInfluence={0.1}
-            noiseAmount={0}
-            distortion={0}
-            pulsating={false}
-            fadeDistance={1}
-            saturation={1}
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.02}
+              noiseAmount={0.0}
+              distortion={0.01}
           />
         </div>
         <main>{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { IEvent } from "@/database/event.model";
-import events from '@/lib/constants';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
