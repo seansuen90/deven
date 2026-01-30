5c34bdf22bf88be727664038069d061d229615ab
seansuen90 <seansuen90@gmail.com>
2026-01-30 08:40:05 +0000

    .

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -34,3 +34,13 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({message: 'Event Creation Failed', error: e instanceof Error ? e.message: 'Unknown'}, {status: 500})
     }
 }
+
+export async function GET() {
+    try {
+        await connectDB();
+        const events = await Event.find().sort({ createdAt: -1 });
+        return NextResponse.json({message: 'Event fetched successfully', events}, {status: 200});
+    } catch (e) {
+        return NextResponse.json({message: 'Event Fetching Failed', error: e}, {status: 500});
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
+import { IEvent } from "@/database/event.model";
 import events from '@/lib/constants';
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
@@ -11,7 +16,7 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none">
-          {events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event} />
             </li>
